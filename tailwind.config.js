7b3d531ed217aee2a3d8de6acf1b00b9ec076ac2
Prince <thedrkprns@gmail.com>
2021-04-15 21:52:57 +0530

    added regex for anti-purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-	purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	purge: {
+	    content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+	    options: {
+	    	safelist: [/(bg|border|text)-\w+-dark/]
+	    }
+	},
 	darkMode: false, // or 'media' or 'class'
 	theme: {
 		extend: {
